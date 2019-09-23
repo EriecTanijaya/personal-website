75c6fe5be55212a9cd9505e0078e994179f705f5
Glitch (sulky-crush) <none>
2019-09-23 07:17:30 +0000

    use express-edge

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const express = require('express');
+const expressEdge = require('express-edge');
 const app = new express();
 
 app.use(express.static('public'));
+app.use(expressEdge);
+
+app.set('views', __dirname + '/views')
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
