20448f93492340f5c036dd38ccfec12e9795aaa8
Glitch (sulky-crush) <none>
2019-09-23 07:43:34 +0000

    using express edge to render

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const express = require('express');
 const expressEdge = require('express-edge');
+
 const app = new express();
 
 app.use(express.static('public'));
-app.use(expressEdge);
+app.use(expressEdge.engine);
 
 app.set('views', __dirname + '/views')
 
