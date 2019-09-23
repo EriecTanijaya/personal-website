53325e499017ee7b4bfe8a2cdded97b68f8fea1f
Glitch (sulky-crush) <none>
2019-09-23 07:06:58 +0000

    pages : add about, contact, post

--- index.js
+++ index.js
@@ -3,8 +3,21 @@ const express = require('express');
 const app = new express();
 
 app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+});
+
+app.get('/post', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/post.html'));
 });
 
 app.listen(4000,() => {
