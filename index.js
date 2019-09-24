ec7b4c112d51306bd34aafa9d4d6038785dfc0fd
Glitch (sulky-crush) <none>
2019-09-24 06:39:09 +0000

    store session in mongo

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const expressSession = require('express-session');
+const connectMongo = require('connect-mongo');
 
 const createPostController = require('./controllers/createPost');
 const homePageController = require('./controllers/homePage');
@@ -24,6 +25,15 @@ const connectionString = process.env.mongo_uri;
 mongoose.connect(connectionString, { useUnifiedTopology: true, useNewUrlParser: true })
     .then(() => console.log('You are now connected to Mongo!'))
     .catch(err => console.error('Something went wrong', err))
+
+const mongoStore = connectMongo(expressSession);
+
+app.use(expressSession({
+  secret: 'secret',
+  store: new mongoStore({
+    mongooseConnection: mongoose.connection
+  })
+}));
 
 app.use(fileUpload());
 app.use(express.static('public'));
