3fdb7912614b8491b05446247f772ba0964ae00d
Glitch (sulky-crush) <none>
2019-09-24 02:06:48 +0000

    add feature to submit post

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const expressEdge = require('express-edge');
 const mongoose = require('mongoose');
 const app = new express();
 const connectionString = process.env.mongo_uri;
+const bodyParser = require('body-parser');
+const Post = require('./database/models/Post');
 
 mongoose.connect(connectionString, { useUnifiedTopology: true, useNewUrlParser: true })
     .then(() => console.log('You are now connected to Mongo!'))
@@ -13,9 +15,27 @@ app.use(express.static('public'));
 app.use(expressEdge.engine);
 
 app.set('views', __dirname + '/views')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await Post.find({})
+  res.render('index', {
+    posts
+  })
+});
+
+app.get('/posts/new', (req, res) => {
+  res.render('create');
+});
+
+app.post('/posts/store', (req, res) => {
+  console.log(req.body);
+  Post.create(req.body, (error, post) => {
+    res.redirect('/')
+  })
 });
 
 app.get('/about', (req, res) => {
