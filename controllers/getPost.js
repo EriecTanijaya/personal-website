5121006d5884d240030142f62d4d8d63f5491c7a
Glitch (sulky-crush) <none>
2019-10-01 08:57:05 +0000

    tampilin delete post hanya untuk login user

--- controllers/getPost.js
+++ controllers/getPost.js
@@ -2,6 +2,7 @@ const Post = require('../database/models/Post')
 
 module.exports = async (req, res) => {
   const post = await Post.findById(req.params.id);
+  const auth = req.session.userId;
   const title = 'WeekyDay Blog | ' + post.title;
-  res.render('post', {post, title});
+  res.render('post', {post, title, auth});
 };
