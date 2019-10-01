d44a7376cea226dc414da70373cba7499d6815c6
Glitch (sulky-crush) <none>
2019-10-01 08:54:05 +0000

    fix title post

--- controllers/getPost.js
+++ controllers/getPost.js
@@ -2,7 +2,6 @@ const Post = require('../database/models/Post')
 
 module.exports = async (req, res) => {
   const post = await Post.findById(req.params.id);
-  res.render('post', {
-    post
-  });
+  const title = 'WeekyDay Blog | ' + post.title;
+  res.render('post', {post, title});
 };
--- todo.md
+++ todo.md
@@ -1,2 +1,4 @@
 [tutorial node js](https://vegibit.com/node-js-blog-tutorial/) to node js blog \
 Rebrand buat minimalis
+
+bisa hapus post sendiri, tapi ga bisa hapus post orang lain
