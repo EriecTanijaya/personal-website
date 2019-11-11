7872a63050bb0db733cd01e76a69deb81a6c7ab0
Glitch (sulky-crush) <none>
2019-11-11 08:52:06 +0000

    add welcome

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -1,7 +1,6 @@
 const Post = require("../database/models/Post");
 
 module.exports = (req, res, next) => {
-  
   var perPage = 5;
   var page = req.params.page || 1;
   var title = "WeekyDay Blog | Home";
@@ -18,7 +17,8 @@ module.exports = (req, res, next) => {
           current: page,
           pages: Math.ceil(count / perPage),
           title: title,
-          auth: req.session.userId
+          auth: req.session.userId,
+          username: req.session.username
         })
       })
     })
