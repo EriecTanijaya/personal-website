ca5ac5cbc8f0667dc8d5c586c0ed63c1543e6e75
Glitch (eriectan) <none>
2019-11-19 02:47:07 +0000

    modularize sitemap controller // TIL express app can be passed as req.app

--- controllers/sitemap.js
+++ controllers/sitemap.js
@@ -1,9 +1,21 @@
-// const express = require("express");
-// const app = new express();
-
 module.exports = (req, res) => {
-
-  
+  // This is just the domain you'll append the routes to
+  const fh = process.env.blog_url;
+  // we extract not null routes
+  const routes = req.app._router.stack
+    .map(r => (r.route ? r.route.path : null))
+    .filter(r => r != null);
+  // parse each routes in an array of array containing all the possible params rotations
+  const realRoutes = parseRoutes(routes);
+  // filter out null values and flatten the array
+  const flattened = flattenArray(realRoutes);
+  // insert it into our XML
+  const sitemapOutput = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+      ${flattened.map(r => `<url><loc>${fh}${r}</loc></url>`).join("")}
+    </urlset>`;
+  // just send it
+  res.status(200).end(sitemapOutput);
 };
 
 function parsePattern(subPattern) {
