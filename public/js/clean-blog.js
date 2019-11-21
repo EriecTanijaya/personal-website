4f6223afb45c08a30b0431f90235af319f131320
Glitch (eriectan) <none>
2019-11-21 04:46:39 +0000

    WORKING theme toggle!! // add toggler

--- public/js/clean-blog.js
+++ public/js/clean-blog.js
@@ -47,4 +47,33 @@
       }
     );
   }
+
+  //checker
+  
+  const toggleSwitch = document.querySelector(
+    '.theme-switch input[type="checkbox"]'
+  );
+
+  const currentTheme = localStorage.getItem("theme");
+
+  if (currentTheme) {
+    document.documentElement.setAttribute("data-theme", currentTheme);
+
+    if (currentTheme === "dark") {
+      toggleSwitch.checked = true;
+    }
+  }
+
+  function switchTheme(e) {
+    if (e.target.checked) {
+      document.documentElement.setAttribute("data-theme", "dark");
+      localStorage.setItem("theme", "dark"); //add this
+    } else {
+      document.documentElement.setAttribute("data-theme", "light");
+      localStorage.setItem("theme", "light"); //add this
+    }
+  }
+  
+  toggleSwitch.addEventListener('change', switchTheme, false);
+  
 })(jQuery); // End of use strict
