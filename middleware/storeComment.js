1898e0b6a81187aed9c7684d2e672b064e3e54cc
Glitch (eriectan) <none>
2019-11-23 04:40:43 +0000

    clean up , backup

--- middleware/storeComment.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res, next) => {
-  req.body.username = req.session.username;
-  
-  if (!req.body.username || !req.body.content) {
-    return res.redirect("/posts/" + req.params.id);
-  }
-
-  next();
-};
