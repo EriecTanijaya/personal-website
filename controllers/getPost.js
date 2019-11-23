c8a26d05f39e5f40543b60ac2be26901b9bbd67e
Glitch (eriectan) <none>
2019-11-23 03:37:51 +0000

    show comment post time

--- controllers/getPost.js
+++ controllers/getPost.js
@@ -7,8 +7,30 @@ module.exports = async (req, res) => {
     const title = "WeekyDay Blog | Kosonk?!";
     return res.render("notFound", { title: title, auth: req.session.userId });
   }
-  console.log("ini di getPost controller", post);
+
   const auth = req.session.username;
   const title = "WeekyDay Blog | " + post.title;
+  let today = new Date();
+  //to compare later
+  today.setHours(0, 0, 0, 0);
+
+  var options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  };
+
+  post.comments.forEach((item, index) => {
+    //default
+    item.time = item.createdAt.toLocaleTimeString("id-ID", {
+      timeZone: "Asia/Jakarta"
+    });
+
+    if (item.createdAt < today) {
+      item.time = item.createdAt.toLocaleDateString("en-EN", options);
+    }
+  });
+
   return res.render("post", { post, title, auth });
 };
