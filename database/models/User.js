d64d11e0c978c8e2fed5f2260944b7a6f94b2ad6
Glitch (eriectan) <none>
2019-11-25 01:36:29 +0000

    fix password not hashed

--- database/models/User.js
+++ database/models/User.js
@@ -18,18 +18,23 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
-const UserModel = mongoose.model("User", UserSchema);
-
+///https://mongoosejs.com/docs/middleware.html
+// Mongoose will call this middleware function, because this script adds
+// the middleware to the schema before compiling the model.
 UserSchema.pre("save", function(next) {
   const user = this;
-
+  console.log("this is pre('save')");
   bcrypt.genSalt(10, function(err, salt) {
     bcrypt.hash(user.password, salt, function(err, hash) {
       user.password = hash;
+      console.log("hash nya", hash);
+      console.log("user password", user.password);
       next();
     });
   });
   
 });
+
+const UserModel = mongoose.model("User", UserSchema);
 
 module.exports = UserModel;
